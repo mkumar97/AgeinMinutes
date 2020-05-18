8fd98ef0b48039428ed1684cdfca5d4d3f4e37e1
mkumar97 <mkumar1@me.iitr.ac.in>
2020-05-18 14:10:02 +0530

    Final Update

--- app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -15,31 +15,44 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btndatepicker.setOnClickListener {
-            view -> clickDatePicker(view)
+        btndatepicker.setOnClickListener { view ->
+            clickDatePicker(view)
             // Toast.makeText(this, "Button Works", Toast.LENGTH_LONG).show()
         }
     }
 
-    private fun clickDatePicker(view: View){
+    private fun clickDatePicker(view: View) {
 
         val myCalender = Calendar.getInstance()
         val year = myCalender.get(Calendar.YEAR)
         val month = myCalender.get(Calendar.MONTH)
         val day = myCalender.get(Calendar.DAY_OF_MONTH)
-        DatePickerDialog(this, DatePickerDialog.OnDateSetListener
-        {    view, selectedYear, selectedMonth, selectedDay ->
-            Toast.makeText(this, "Date Picker Works; Selected $selectedYear/${selectedMonth+1}/$selectedDay", Toast.LENGTH_LONG).show()
-            val selectedDate = "$selectedDay/${selectedMonth+1}/$selectedYear"
-            selecteddate.setText(selectedDate)
-            val formattedDate = SimpleDateFormat("dd/MM/YYYY", Locale.ENGLISH)
-            val parsedDate = formattedDate.parse(selectedDate)
-            val minutespassed = parsedDate!!.time / (1000) // in milli seconds and !! makes sure that input is not null
-            val currentDate = formattedDate.parse(formattedDate.format(System.currentTimeMillis()))
-            val totalminutes = currentDate!!.time / (1000)
-            val minutesWasted = totalminutes - minutespassed
-            selecteddateinmin.setText(minutesWasted.toString())
-        }, year, month, day).show() // year, month, day which are passed decides which date to show when dailog opens, not selected
+        val dpd = DatePickerDialog(
+            this, DatePickerDialog.OnDateSetListener
+            { view, selectedYear, selectedMonth, selectedDay ->
+                Toast.makeText(
+                    this,
+                    "Date Picker Works; Selected $selectedYear/${selectedMonth + 1}/$selectedDay",
+                    Toast.LENGTH_LONG
+                ).show()
+                val selectedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear"
+                selecteddate.setText(selectedDate)
+                val formattedDate = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+                val parsedDate = formattedDate.parse(selectedDate)
+                val minutespassed =
+                    parsedDate!!.time / (60000) // in milli seconds and !! makes sure that input is not null
+                val currentDate =
+                    formattedDate.parse(formattedDate.format(System.currentTimeMillis()))
+                val totalminutes = currentDate!!.time / (60000)
+                val minutesWasted = totalminutes - minutespassed
+                selecteddateinmin.setText(minutesWasted.toString())
+            }, year, month, day
+        ) // year, month, day which are passed decides which date to show when dailog opens, not selected
         // Toast.makeText(this, "Date Picker Works", Toast.LENGTH_LONG).show()
+
+        dpd.datePicker.setMaxDate(Date().time - 86400000)
+        dpd.show()
     }
+
+
 }
